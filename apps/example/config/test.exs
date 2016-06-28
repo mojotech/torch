655f24aa891047d2bd6cfd4d97a713a755febf4b
Daniel Berkompas <himself@danielberkompas.com>
2016-06-28 10:25:55 -0700

    Configure database for Semaphore

--- apps/example/config/test.exs
+++ apps/example/config/test.exs
@@ -9,9 +9,15 @@ config :example, Example.Endpoint,
 # Print only warnings and errors during test
 config :logger, level: :warn
 
-# Configure your database
+# Set up test project
+{whoami, _} = System.cmd("whoami", [])
+whoami = String.replace(whoami, "\n", "")
+
 config :example, Example.Repo,
   adapter: Ecto.Adapters.Postgres,
   database: "example_test",
+  username: System.get_env("DATABASE_POSTGRESQL_USERNAME") || whoami,
+  password: System.get_env("DATABASE_POSTGRESQL_PASSWORD") || nil,
   hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
+  pool: Ecto.Adapters.SQL.Sandbox,
+  ownership_timeout: 60_000
