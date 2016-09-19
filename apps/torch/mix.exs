9da2a1fe7cf625d941dc2ae9c9feca811b756c84
Daniel Berkompas <himself@danielberkompas.com>
2016-09-19 14:42:31 -0700

    Bump to v1.0.0-rc.3

--- apps/torch/mix.exs
+++ apps/torch/mix.exs
@@ -3,7 +3,7 @@ defmodule Torch.Mixfile do
 
   def project do
     [app: :torch,
-     version: "1.0.0-rc.2",
+     version: "1.0.0-rc.3",
      build_path: "../../_build",
      config_path: "../../config/config.exs",
      deps_path: "../../deps",
