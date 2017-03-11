1dd06b25b8cf9759cbe3749096dc17d0181749ac
Daniel Berkompas <himself@danielberkompas.com>
2017-03-11 15:12:02 -0800

    [#13] Namespace normalize CSS

--- example/mix.exs
+++ example/mix.exs
@@ -38,7 +38,7 @@ defmodule Example.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:torch, path: "../.."},
+     {:torch, path: "../"},
      {:phoenix_slime, "~> 0.6.0"}]
   end
 
