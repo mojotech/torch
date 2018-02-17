c1da56b75ab1ee5e3fa2e60cc7027b7e474622fb
Zachary Berkompas <zberkom@gmail.com>
2018-02-16 17:17:08 -0800

    :white_check_mark: Update test database creds

--- example/config/test.exs
+++ example/config/test.exs
@@ -12,8 +12,8 @@ config :logger, level: :warn
 # Configure your database
 config :example, Example.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: System.get_env("DATABASE_POSTGRESQL_USERNAME") || whoami,
+  password: System.get_env("DATABASE_POSTGRESQL_PASSWORD") || "",
   database: "example_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
