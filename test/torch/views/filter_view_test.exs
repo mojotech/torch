f803144e7dce8fb88946f00f851d4e4b09351016
Daniel Berkompas <himself@danielberkompas.com>
2018-04-26 11:45:07 -0700

    Add tests to Torch.FilterView

--- test/torch/views/filter_view_test.exs
+++ test/torch/views/filter_view_test.exs
@@ -2,11 +2,5 @@ defmodule Torch.FilterViewTest do
   @moduledoc false
   use ExUnit.Case
 
-  alias Torch.FilterView
-
-  describe "filter_date_input" do
-    test "renders succesfully" do
-      assert {:safe, _} = FilterView.filter_date_input(:post, :inserted_at, %{})
-    end
-  end
+  doctest Torch.FilterView, import: true
 end
