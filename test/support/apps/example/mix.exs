2b4ec3cb553fbef65df0cc6346b6a8d30702e530
Daniel Berkompas <himself@danielberkompas.com>
2018-04-26 13:17:18 -0700

    Tweak dependencies

--- test/support/apps/example/mix.exs
+++ test/support/apps/example/mix.exs
@@ -35,7 +35,7 @@ defmodule Example.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3.0"},
+      {:phoenix, "~> 1.3"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.2"},
       {:postgrex, ">= 0.0.0"},
