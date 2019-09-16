6f21df4d3159f4dac3df76ae689614bfaa57abfd
Craig P Jolicoeur <craig@mojotech.com>
2019-09-16 09:42:49 -0400

    Release version v2.0.0-rc.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Torch.MixProject do
   def project do
     [
       app: :torch,
-      version: "2.0.0",
+      version: "2.0.0-rc.3",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
