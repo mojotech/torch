f9c4903d65a79ae1dc2a1ef55d38fb163fbb97e3
Craig P Jolicoeur <craig@mojotech.com>
2020-04-13 11:33:15 -0400

    [test] Fix properly resetting I18n backend at test exit

--- test/torch/i18n_test.exs
+++ test/torch/i18n_test.exs
@@ -8,7 +8,7 @@ defmodule Torch.I18nTest do
 
   setup_all do
     on_exit(fn ->
-      Application.put_env(:torch, :i18n_backend, CustomI18nBackend)
+      Application.put_env(:torch, :i18n_backend, Torch.I18N.Backend)
     end)
 
     [i18n_backend: Torch.Config.i18n_backend()]
