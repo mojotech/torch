649de7ef4595c1c84c8b0977e2f7972dbd0e10c9
Craig P Jolicoeur <craig@mojotech.com>
2020-04-17 11:53:09 -0400

    [config] Version bump for next release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Torch.MixProject do
   def project do
     [
       app: :torch,
-      version: "3.1.0",
+      version: "3.2.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
