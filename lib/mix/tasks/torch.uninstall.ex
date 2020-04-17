f3c2eda984d6057b127fdd583ea3b5f73dde9b0b
Craig P Jolicoeur <craig@mojotech.com>
2020-04-17 12:00:12 -0400

    Don't run torch.gen tasks at top-level of umbrella
    When in an umbrella app, raise an error if any of the
    torch.gen.* Mix tasks are run at the top-level instead of
    in the app-specific level directories.

--- lib/mix/tasks/torch.uninstall.ex
+++ lib/mix/tasks/torch.uninstall.ex
@@ -8,6 +8,10 @@ defmodule Mix.Tasks.Torch.Uninstall do
   """
 
   def run(args) do
+    if Mix.Project.umbrella?() do
+      Mix.raise("mix torch.uninstall can only be run inside an application directory")
+    end
+
     %{format: format, otp_app: otp_app} = Mix.Torch.parse_config!("torch.uninstall", args)
 
     paths = [
