37b535e07d1cc8efdafcdec33ea751fbd2275392
Yaroslav Zemlyanuhin <y.zemlyanuhin@cti.ru>
2021-06-10 12:09:20 +0300

    Test templates updated with best-practice describes

--- priv/templates/phx.gen.context/test_cases.exs
+++ priv/templates/phx.gen.context/test_cases.exs
@@ -1,21 +1,12 @@
 
-  describe "<%= schema.plural %>" do
-    alias <%= inspect schema.module %>
+  alias <%= inspect schema.module %>
 
-    @valid_attrs <%= inspect schema.params.create %>
-    @update_attrs <%= inspect schema.params.update %>
-    @invalid_attrs <%= inspect for {key, _} <- schema.params.create, into: %{}, do: {key, nil} %>
+  @valid_attrs <%= inspect schema.params.create %>
+  @update_attrs <%= inspect schema.params.update %>
+  @invalid_attrs <%= inspect for {key, _} <- schema.params.create, into: %{}, do: {key, nil} %>
 
-    def <%= schema.singular %>_fixture(attrs \\ %{}) do
-      {:ok, <%= schema.singular %>} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> <%= inspect context.alias %>.create_<%= schema.singular %>()
-
-      <%= schema.singular %>
-    end
-
-    test "paginate_<%= schema.plural %>/1 returns paginated list of <%= schema.plural %>" do
+  describe "#paginate_<%= schema.plural %>/1" do
+    test "returns paginated list of <%= schema.plural %>" do
       for _ <- 1..20 do
         <%= schema.singular %>_fixture()
       end
@@ -31,47 +22,69 @@
       assert page.sort_field == "inserted_at"
       assert page.sort_direction == "desc"
     end
+  end
 
-    test "list_<%= schema.plural %>/0 returns all <%= schema.plural %>" do
+  describe "#list_<%= schema.plural %>/0" do
+    test "returns all <%= schema.plural %>" do
       <%= schema.singular %> = <%= schema.singular %>_fixture()
       assert <%= inspect context.alias %>.list_<%= schema.plural %>() == [<%= schema.singular %>]
     end
+  end
 
-    test "get_<%= schema.singular %>!/1 returns the <%= schema.singular %> with given id" do
+  describe "#get_<%= schema.singular %>!/1" do
+    test "returns the <%= schema.singular %> with given id" do
       <%= schema.singular %> = <%= schema.singular %>_fixture()
       assert <%= inspect context.alias %>.get_<%= schema.singular %>!(<%= schema.singular %>.id) == <%= schema.singular %>
     end
+  end
 
-    test "create_<%= schema.singular %>/1 with valid data creates a <%= schema.singular %>" do
+  describe "#create_<%= schema.singular %>/1" do
+    test "with valid data creates a <%= schema.singular %>" do
       assert {:ok, %<%= inspect schema.alias %>{} = <%= schema.singular %>} = <%= inspect context.alias %>.create_<%= schema.singular %>(@valid_attrs)<%= for {field, value} <- schema.params.create do %>
       assert <%= schema.singular %>.<%= field %> == <%= Mix.Phoenix.Schema.value(schema, field, value) %><% end %>
     end
 
-    test "create_<%= schema.singular %>/1 with invalid data returns error changeset" do
+    test "with invalid data returns error changeset" do
       assert {:error, %Ecto.Changeset{}} = <%= inspect context.alias %>.create_<%= schema.singular %>(@invalid_attrs)
     end
+  end
 
-    test "update_<%= schema.singular %>/2 with valid data updates the <%= schema.singular %>" do
+  describe "#update_<%= schema.singular %>/2" do
+    test "with valid data updates the <%= schema.singular %>" do
       <%= schema.singular %> = <%= schema.singular %>_fixture()
       assert {:ok, <%= schema.singular %>} = <%= inspect context.alias %>.update_<%= schema.singular %>(<%= schema.singular %>, @update_attrs)
       assert %<%= inspect schema.alias %>{} = <%= schema.singular %><%= for {field, value} <- schema.params.update do %>
       assert <%= schema.singular %>.<%= field %> == <%= Mix.Phoenix.Schema.value(schema, field, value) %><% end %>
     end
 
-    test "update_<%= schema.singular %>/2 with invalid data returns error changeset" do
+    test "with invalid data returns error changeset" do
       <%= schema.singular %> = <%= schema.singular %>_fixture()
       assert {:error, %Ecto.Changeset{}} = <%= inspect context.alias %>.update_<%= schema.singular %>(<%= schema.singular %>, @invalid_attrs)
       assert <%= schema.singular %> == <%= inspect context.alias %>.get_<%= schema.singular %>!(<%= schema.singular %>.id)
     end
+  end
 
-    test "delete_<%= schema.singular %>/1 deletes the <%= schema.singular %>" do
+  describe "#delete_<%= schema.singular %>/1" do
+    test "deletes the <%= schema.singular %>" do
       <%= schema.singular %> = <%= schema.singular %>_fixture()
       assert {:ok, %<%= inspect schema.alias %>{}} = <%= inspect context.alias %>.delete_<%= schema.singular %>(<%= schema.singular %>)
       assert_raise Ecto.NoResultsError, fn -> <%= inspect context.alias %>.get_<%= schema.singular %>!(<%= schema.singular %>.id) end
     end
+  end
 
-    test "change_<%= schema.singular %>/1 returns a <%= schema.singular %> changeset" do
+  describe "#change_<%= schema.singular %>/1" do
+    test "returns a <%= schema.singular %> changeset" do
       <%= schema.singular %> = <%= schema.singular %>_fixture()
       assert %Ecto.Changeset{} = <%= inspect context.alias %>.change_<%= schema.singular %>(<%= schema.singular %>)
     end
   end
+
+  def <%= schema.singular %>_fixture(attrs \\ %{}) do
+    {:ok, <%= schema.singular %>} =
+      attrs
+      |> Enum.into(@valid_attrs)
+      |> <%= inspect context.alias %>.create_<%= schema.singular %>()
+
+    <%= schema.singular %>
+  end
+
