da3214083a9dae0be0302f113be42a793c291630
Craig P Jolicoeur <craig@mojotech.com>
2021-06-22 09:31:52 -0400

    Fix bug with multiple filters w/ the same prefix
    * See GitHub Issue 242: https://github.com/mojotech/torch/issues/242
    
    [close #242]

--- test/torch/views/filter_view_test.exs
+++ test/torch/views/filter_view_test.exs
@@ -1,7 +1,28 @@
 defmodule Torch.FilterViewTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
   import Phoenix.HTML, only: [safe_to_string: 1]
 
   doctest Torch.FilterView, import: true
+
+  test "param name prefixes do not collide" do
+    # * See GitHub Issue 242: https://github.com/mojotech/torch/issues/242
+
+    params = %{
+      "filters" => [
+        "user[email_contains]",
+        "user[first_name_contains]",
+        "user[last_name_contains]",
+        "user[id_equals]"
+      ],
+      "user" => %{
+        "email_confirmed_at_between" => %{"end" => "2021-06-03", "start" => "2021-06-08"}
+      }
+    }
+
+    expected = "<input id=\"user_email_contains\" name=\"user[email_contains]\" type=\"text\">"
+
+    assert expected ==
+             safe_to_string(Torch.FilterView.filter_string_input(:user, :email, params))
+  end
 end
