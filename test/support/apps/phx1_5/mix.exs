76123fee2d162d38722774718665236059aab5d1
Craig P Jolicoeur <craig@mojotech.com>
2021-07-01 14:18:18 -0400

    Update phx1_5 test app to latest 1.5.9

--- test/support/apps/phx1_5/mix.exs
+++ test/support/apps/phx1_5/mix.exs
@@ -33,7 +33,7 @@ defmodule Phx15.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.1"},
+      {:phoenix, "~> 1.5.9"},
       {:phoenix_ecto, "~> 4.1"},
       {:ecto_sql, "~> 3.4"},
       {:postgrex, ">= 0.0.0"},
