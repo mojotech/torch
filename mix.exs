bbb7d9a312479aa73d4028e5bc1d037e63dff123
Craig P Jolicoeur <craig@mojotech.com>
2021-09-15 11:05:52 -0400

    Support phx 1.6

--- mix.exs
+++ mix.exs
@@ -43,8 +43,8 @@ defmodule Torch.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:phoenix, ">= 1.3.0 and < 1.6.0"},
-      {:phoenix_html, "~> 2.10"},
+      {:phoenix, ">= 1.3.0 and < 1.7.0"},
+      {:phoenix_html, "~> 3.0"},
       {:gettext, "~> 0.16"},
       {:scrivener_ecto, ">= 1.2.1"},
       {:filtrex, "~> 0.4.1"},
