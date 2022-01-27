f00a9c9a04aa53c8509653e5e1ab3cc2285a252a
Will Cohen <chewnoill@gmail.com>
2022-01-27 08:32:14 -0500

    [chore] migrate to to 1.6

--- test/support/apps/phx1_5/lib/phx1_5/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule Phx15.Repo do
-  use Ecto.Repo,
-    otp_app: :phx1_5,
-    adapter: Ecto.Adapters.Postgres
-end
--- test/support/apps/phx1_5/lib/phx1_5_web/controllers/page_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule Phx15Web.PageController do
-  use Phx15Web, :controller
-
-  def index(conn, _params) do
-    render(conn, "index.html")
-  end
-end
