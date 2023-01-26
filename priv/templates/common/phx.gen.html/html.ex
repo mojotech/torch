c0b2cf3e1e7673c04f8ade0e9f5055f2ad22f2bc
Craig P Jolicoeur <craig@mojotech.com>
2023-01-26 15:30:45 -0500

    [WIP] Add HTML template and drop View template
    Phoenix.View doesnt exist in core Phoenix 1.7.  Drop the
    View file and add the *HTML template instead

---
+++ priv/templates/common/phx.gen.html/html.ex
@@ -0,0 +1,8 @@
+defmodule <%= inspect context.web_module %>.<%= inspect Module.concat(schema.web_namespace, schema.alias) %>HTML do
+  use <%= inspect context.web_module %>, :html
+
+  embed_templates "<%= schema.singular %>_html/*"
+
+  import Torch.TableView
+  import Torch.FilterView
+end
