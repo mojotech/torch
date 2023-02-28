033d1f29350430eb832dcf84ee6ac91b0d04db26
Adam Millerchip <adam@millerchip.net>
2023-02-28 15:59:07 +0900

    Remove usage of deprecated get_flash/2

--- lib/torch/views/flash_view.ex
+++ lib/torch/views/flash_view.ex
@@ -6,8 +6,6 @@ defmodule Torch.FlashView do
   use Phoenix.View, root: "lib/torch/templates"
   use Phoenix.HTML
 
-  import Phoenix.Controller, only: [get_flash: 2]
-
   @doc """
   Returns a formatted flash message of the given type.
 
@@ -18,13 +16,13 @@ defmodule Torch.FlashView do
 
   ## Example
 
-      iex> conn = %Plug.Conn{private: %{phoenix_flash: %{"error" => "Error Message"}}}
+      iex> conn = %Plug.Conn{assigns: %{flash: %{"error" => "Error Message"}}}
       ...> flash_message(conn, "error") |> safe_to_string()
       "<p class=\\"torch-flash error\\">Error Message <button class='torch-flash-close'>x</button></p>"
   """
   @spec flash_message(Plug.Conn.t(), type :: atom | String.t()) :: Phoenix.HTML.safe()
   def flash_message(conn, type) do
-    message = get_flash(conn, type)
+    message = Phoenix.Flash.get(conn.assigns[:flash] || %{}, type)
 
     if message do
       content_tag :p, class: "torch-flash #{type}" do
