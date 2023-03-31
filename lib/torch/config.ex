c2931ae577774532487685aec896bdc3dc088a96
Craig P Jolicoeur <craig@mojotech.com>
2023-03-31 11:46:38 -0400

    [config] Remove obsolete config references
    The `template_format` config option is no longer
    used or required so remove all references to it.

--- lib/torch/config.ex
+++ lib/torch/config.ex
@@ -7,10 +7,6 @@ defmodule Torch.Config do
     Application.get_env(:torch, :otp_app)
   end
 
-  def template_format do
-    "heex"
-  end
-
   def i18n_backend do
     Application.get_env(:torch, :i18n_backend, Torch.I18n.Backend)
   end
