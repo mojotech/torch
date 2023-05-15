26c50d49b0c81ff2654aefe10554a7121b6bf2c7
Craig P Jolicoeur <craig@mojotech.com>
2023-05-15 11:53:43 -0400

    Migrate PageView to Torch.Helpers
    There is no need to have a dedicated Torch.PageView
    module as this module only provided a minimal public
    helper function.  This commit deprecates the `body_classes/1`
    function from Torch.PageView and relocates the functionality
    to the new `Torch.Helpers.body_classes/1` function.

--- lib/torch/helpers.ex
+++ lib/torch/helpers.ex
@@ -120,4 +120,32 @@ defmodule Torch.Helpers do
         params
     end
   end
+
+  @doc """
+  Takes the controller action name and appends it to the torch- prefix.
+
+  ## Example
+
+      iex> body_classes(%Plug.Conn{private: %{phoenix_action: :create}})
+      "torch-create"
+
+      iex> body_classes(%Plug.Conn{private: %{phoenix_action: :custom_action}})
+      "torch-custom-action"
+  """
+  @spec body_classes(Plug.Conn.t()) :: String.t()
+  def body_classes(conn) do
+    conn
+    |> action_name()
+    |> add_torch_prefix()
+    |> String.trim()
+  end
+
+  defp action_name(conn) do
+    conn
+    |> Phoenix.Controller.action_name()
+    |> Atom.to_string()
+    |> String.replace("_", "-")
+  end
+
+  defp add_torch_prefix(str), do: "torch-#{str}"
 end
