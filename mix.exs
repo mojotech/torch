5bf638080346a306211ea5e1b89e8bfd8e7c8207
Craig P Jolicoeur <craig@mojotech.com>
2024-05-20 13:21:25 -0400

    [hex] Update hackney

--- mix.exs
+++ mix.exs
@@ -54,7 +54,8 @@ defmodule Torch.MixProject do
       {:credo, "~> 1.1", only: [:dev, :test]},
       {:ex_doc, ">= 0.0.0", only: [:dev, :test], runtime: false},
       {:ex_unit_notifier, "~> 1.0", only: [:test]},
-      {:mix_test_watch, "~> 1.0", only: [:dev], runtime: false}
+      {:mix_test_watch, "~> 1.0", only: [:dev], runtime: false},
+      {:ssl_verify_fun, manager: :rebar3, runtime: false, override: true}
     ]
   end
 
